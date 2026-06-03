9da1d38ea498d31d4f51b38912e86ff60015ea05
William Cotton <williamc@eurekasoft.com>
2026-06-03 15:27:58 -0500

    v0.19.0

---
+++ Formula/pdl.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# Algraf formula for Homebrew
+class Pdl < Formula
+  desc 'Pipeline Data Language'
+  homepage 'https://github.com/williamcotton/pdl'
+  url 'https://github.com/williamcotton/pdl.git', tag: 'v0.19.0'
+  license 'MIT'
+  head 'https://github.com/williamcotton/pdl.git', branch: 'main'
+
+  depends_on 'rust' => :build
+
+  def install
+    system 'cargo', 'install', *std_cargo_args(path: 'crates/pdl-cli')
+  end
+
+  test do
+    assert_match 'pdl', shell_output("#{bin}/pdl --version")
+  end
+end
